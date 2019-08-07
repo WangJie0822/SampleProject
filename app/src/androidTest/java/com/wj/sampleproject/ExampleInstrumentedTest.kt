ffb9cbf14f7f9610c9bf935095ef14b32950d954
王杰 <wangjie-yf@log56.com>
2019-08-07 11:01:34 +0800

    项目新建

---
+++ app/src/androidTest/java/com/wj/sampleproject/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.wj.sampleproject
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        assertEquals("com.arcb.laborunion", appContext.packageName)
+    }
+}
+
