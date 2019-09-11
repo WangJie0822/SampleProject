3940535768174b285bc97266d17f6b72a258d6d6
王杰 <wangjie-yf@log56.com>
2019-09-11 09:12:38 +0800

    首页Banner

--- app/src/main/kotlin/com/wj/sampleproject/fragment/HomepageFragment.kt
+++ app/src/main/kotlin/com/wj/sampleproject/fragment/HomepageFragment.kt
@@ -3,6 +3,7 @@ package com.wj.sampleproject.fragment
 import androidx.lifecycle.Observer
 import cn.wj.android.recyclerview.layoutmanager.WrapContentLinearLayoutManager
 import com.wj.sampleproject.R
+import com.wj.sampleproject.adapter.BannerVpAdapter
 import com.wj.sampleproject.adapter.HomepageArticleListRvAdapter
 import com.wj.sampleproject.base.ui.BaseFragment
 import com.wj.sampleproject.databinding.AppFragmentHomepageBinding
@@ -31,16 +32,32 @@ class HomepageFragment
 
     override val layoutResID: Int = R.layout.app_fragment_homepage
 
+    /** Banner 列表适配器 */
+    private val mBannerAdapter: BannerVpAdapter by inject()
     /** 文章列表适配器 */
     private val mArticlesAdapter: HomepageArticleListRvAdapter by inject()
 
     override fun initView() {
+        // 配置 Banner 列表
+        mBannerAdapter.mViewModel = mViewModel
+        mBinding.vpBanner.adapter = mBannerAdapter
+
         // 配置文章列表
+        mArticlesAdapter.mViewModel = mViewModel
         mBinding.rvArticles.layoutManager = WrapContentLinearLayoutManager()
         mBinding.rvArticles.adapter = mArticlesAdapter
 
         // 添加观察者
+        // Banner 列表
+        mViewModel.bannerData.observe(this, Observer {
+            // 更新 Banner 列表
+            mBannerAdapter.refresh(it)
+            // 设置 Banner 数量并开启轮播
+            mViewModel.bannerCount = it.size
+        })
+        // 文章列表
         mViewModel.articleListData.observe(this, Observer {
+            // 更新文章列表
             mArticlesAdapter.loadData(it.list, it.refresh)
         })
     }
--- app/src/main/kotlin/com/wj/sampleproject/mvvm/HomepageViewModel.kt
+++ app/src/main/kotlin/com/wj/sampleproject/mvvm/HomepageViewModel.kt
@@ -1,21 +1,27 @@
 package com.wj.sampleproject.mvvm
 
 import android.view.MenuItem
+import android.view.MotionEvent
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import cn.wj.android.base.databinding.BindingField
+import cn.wj.android.base.ext.condition
+import cn.wj.android.base.ext.isNotNullAndEmpty
+import cn.wj.android.base.ext.orEmpty
 import cn.wj.android.base.utils.AppManager
 import com.wj.sampleproject.R
 import com.wj.sampleproject.activity.SearchActivity
 import com.wj.sampleproject.base.SnackbarEntity
 import com.wj.sampleproject.base.mvvm.BaseViewModel
+import com.wj.sampleproject.constants.MAIN_BANNER_TRANSFORM_INTERVAL_MS
 import com.wj.sampleproject.constants.NET_PAGE_START
 import com.wj.sampleproject.entity.ArticleEntity
+import com.wj.sampleproject.entity.BannerEntity
 import com.wj.sampleproject.ext.showMsg
 import com.wj.sampleproject.net.RefreshList
 import com.wj.sampleproject.repository.HomepageRepository
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 /**
  * 主界面 - 首页 ViewModel
@@ -32,8 +38,22 @@ constructor(private val repository: HomepageRepository)
     override fun onCreate(source: LifecycleOwner) {
         super.onCreate(source)
 
+        // 获取 Banner 数据
+        getHomepageBannerList()
         // 刷新文章列表
         refreshing.set(true)
+    }
+
+    override fun onStart(source: LifecycleOwner) {
+        super.onStart(source)
+        // 开启轮播
+        startCarousel()
+    }
+
+    override fun onStop(source: LifecycleOwner) {
+        super.onStop(source)
+        // 关闭轮播
+        stopCarousel()
     }
 
     /** 菜单列表点击 */
@@ -44,8 +64,42 @@ constructor(private val repository: HomepageRepository)
         false
     }
 
+    /** 标记 - 是否显示 Banner */
+    val showBanner: BindingField<Boolean> = BindingField(false)
+
+    /** Banner 下标 */
+    val bannerCurrent: BindingField<Int> = BindingField()
+
+    /** Banner 预加载页数 */
+    val bannerLimit: BindingField<Int> = BindingField()
+
+    /** Banner 数量 */
+    var bannerCount: Int = 0
+        set(value) {
+            field = value
+            // 更新 Banner 预加载页数
+            bannerLimit.set(field * 2)
+            // 开启轮询
+            startCarousel()
+        }
+
+    /** Banner 触摸事件 */
+    val onBannerTouch: (MotionEvent) -> Boolean = { event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                // 按下、移动，取消轮播
+                stopCarousel()
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                // 抬起、取消，开启轮播
+                startCarousel()
+            }
+        }
+        false
+    }
+
     /** 标记 - 是否正在刷新 */
-    val refreshing: BindingField<Boolean> = BindingField(false)
+    val refreshing: BindingField<Boolean> = BindingField(true)
 
     /** 刷新回调 */
     val onRefresh: () -> Unit = {
@@ -65,8 +119,70 @@ constructor(private val repository: HomepageRepository)
     /** 页码 */
     private var pageNum = NET_PAGE_START
 
+    /** Banner 列表数据 */
+    val bannerData = MutableLiveData<ArrayList<BannerEntity>>()
     /** 文章列表数据 */
     val articleListData = MutableLiveData<RefreshList<ArticleEntity>>()
+
+    /** Banner 轮播 job */
+    private var carouselJob: Job? = null
+
+    /**
+     * 获取首页 Banner 列表
+     */
+    private fun getHomepageBannerList() {
+        viewModelScope.launch {
+            try {
+                // 获取 Banner 数据
+                val result = repository.getHomepageBannerList()
+                if (result.success()) {
+                    // 请求成功
+                    showBanner.set(result.data.isNotNullAndEmpty())
+                    bannerData.postValue(result.data.orEmpty())
+                } else {
+                    snackbarData.postValue(SnackbarEntity(result.errorMsg))
+                    showBanner.set(false)
+                }
+            } catch (throwable: Throwable) {
+                snackbarData.postValue(SnackbarEntity(throwable.showMsg))
+                showBanner.set(false)
+            }
+        }
+    }
+
+    /**
+     * 开启 Banner 轮播
+     */
+    private fun startCarousel() {
+        // 关闭轮播
+        stopCarousel()
+        if (bannerCount < 2) {
+            // Banner 小于2，不轮播
+            return
+        }
+        // 新建协程
+        carouselJob = viewModelScope.launch(Dispatchers.IO) {
+            while (isActive) {
+                // 延时
+                delay(MAIN_BANNER_TRANSFORM_INTERVAL_MS)
+                // 切换
+                val current = bannerCurrent.get() ?: 0
+                bannerCurrent.set((current + 1) % bannerCount)
+            }
+        }
+    }
+
+    /**
+     * 关闭 Banner 轮播
+     */
+    private fun stopCarousel() {
+        if (carouselJob != null) {
+            if (carouselJob?.isActive.condition) {
+                carouselJob?.cancel()
+            }
+            carouselJob = null
+        }
+    }
 
     /**
      * 获取首页文章列表
