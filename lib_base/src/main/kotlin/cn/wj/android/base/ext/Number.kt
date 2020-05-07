3dcc57b9b0cbf5e66826169120563000dccfe1fe
王杰 <15555650921@163.com>
2020-05-07 20:25:27 +0800

    尺寸单位转换问题修改

--- lib_base/src/main/kotlin/cn/wj/android/base/ext/Number.kt
+++ lib_base/src/main/kotlin/cn/wj/android/base/ext/Number.kt
@@ -3,7 +3,11 @@
 
 package cn.wj.android.base.ext
 
-import cn.wj.android.base.tools.*
+import cn.wj.android.base.tools.dip2px
+import cn.wj.android.base.tools.getColorById
+import cn.wj.android.base.tools.getStringById
+import cn.wj.android.base.tools.sp2px
+import kotlin.math.roundToInt
 
 /**
  * 资源相关拓展
@@ -15,15 +19,15 @@ import cn.wj.android.base.tools.*
 
 /** 单位标记 - DP 单位 */
 val Number.dp: Float
-    get() = px2dp(this)
+    get() = dip2px(this)
 
 /** 单位标记 - SP */
 val Number.sp: Float
-    get() = px2sp(this)
+    get() = sp2px(this)
 
 /** 单位标记 - DP 单位 */
 val Number.dpi: Int
-    get() = px2dpi(this)
+    get() = dip2px(this).roundToInt()
 
 /** String 字符串 */
 val Int.string: String
